03ed2ca407bd3bd13badcbc11a8ddef33b4fdeca
bhanu <bhanu@C02WQ20EHTD6.local>
2019-12-07 04:07:36 -0500

    snli scripts

--- scripts/snli.sh
+++ scripts/snli.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 python run_snli.py --data_dir=/mnt/nfs/work1/mfiterau/brawat/snli/data/snli_1.0 \
---model_type=bert \
+--model_=bert-base-uncased
+--model_name_or_path=bert \
 --language=english \
 --output_dir=/mnt/nfs/work1/mfiterau/brawat/snli/out \
 --cache=/mnt/nfs/work1/mfiterau/brawat/snli/cache \
