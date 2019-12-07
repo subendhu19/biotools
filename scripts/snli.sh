d46a1ff33cb29ce35ac35e2f6c2499da3c573e70
bhanu <bhanu@C02WQ20EHTD6.local>
2019-12-07 04:05:10 -0500

    snli scripts

--- scripts/snli.sh
+++ scripts/snli.sh
@@ -6,6 +6,6 @@ python run_snli.py --data_dir=/mnt/nfs/work1/mfiterau/brawat/snli/data/snli_1.0 
 --output_dir=/mnt/nfs/work1/mfiterau/brawat/snli/out \
 --cache=/mnt/nfs/work1/mfiterau/brawat/snli/cache \
 --max_seq_length=200 \
---do_lower_case=True \
---do_train=True \
---do_eval=True
+--do_lower_case \
+--do_train \
+--do_eval
